a014ed58461f47a32a6502e495b9303bd5c000f1
Sergio Herrero <movilsergio2000@gmail.com>
2025-02-02 01:53:19 +0100

    Add global messages support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { UpperCasePipe } from '@angular/common';
 import {
-    Component,
-    ElementRef,
-    HostListener,
-    OnInit,
-    Renderer2,
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  Renderer2,
 } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TranslocoPipe } from '@jsverse/transloco';
 import { EjdaUserLogoutComponent } from './components/auth/user-logout/user-logout.component';
 import { EjdaLanguageSelectorComponent } from './components/language-selector/language-selector.component';
+import { EjdaGlobalMessageComponent } from './shared/services/global-message-service/component/global-message.component';
 
 @Component({
   selector: 'app-root',
@@ -19,6 +20,7 @@ import { EjdaLanguageSelectorComponent } from './components/language-selector/la
     TranslocoPipe,
     EjdaLanguageSelectorComponent,
     EjdaUserLogoutComponent,
+    EjdaGlobalMessageComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
